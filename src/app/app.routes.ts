69aaa92809eae4ebe1d18adf18e1d2c511be3343
Mafe Parra Casas <mfeparra@poligran.edu.co>
2024-05-31 21:50:21 -0500

    DeliAPP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,25 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+  },
+
+  {
+    path: 'compras',
+    loadComponent: () => import('./compras/compras.page').then( m => m.ComprasPage)
+  },
+  {
+    path: 'mi-perfil',
+    loadComponent: () => import('./mi-perfil/mi-perfil.page').then( m => m.MiPerfilPage)
+  },
+  {
+    path: 'restaurantes',
+    loadComponent: () => import('./restaurantes/restaurantes.page').then( m => m.RestaurantesPage)
+  },
+  {
+    path: 'info',
+    loadComponent: () => import('./info/info.page').then( m => m.InfoPage)
+  },
 ];
