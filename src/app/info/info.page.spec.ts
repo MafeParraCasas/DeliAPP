9282322f9d37d395f568ab40d68eec0f461cef51
MafeParraCasas <mfeparra@poligran.edu.co>
2024-06-06 20:00:25 -0500

    master

--- src/app/info/info.page.spec.ts
+++ src/app/info/info.page.spec.ts
@@ -15,3 +15,5 @@ describe('InfoPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonAvatar,IonSearchbar} from '@ionic/angular/standalone';
+import { IonLabel,IonBreadcrumb,IonIcon,IonBreadcrumbs} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton,IonAvatar,IonSearchbar,IonLabel,IonBreadcrumb,IonIcon,IonBreadcrumbs]
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    
+    addIcons({ });
+  }
 
   ngOnInit() {
   }
