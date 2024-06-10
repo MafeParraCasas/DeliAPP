bf6ed6fec5a4d04064eef1876958c68c51abbee2
Jesus Lobaton <101685851+JesusAL93@users.noreply.github.com>
2024-06-09 22:01:42 -0500

    configuración de los productos

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../interfaces/product';
 import { CartService } from '../services/cart.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar ,IonButtons, IonMenuButton, IonItem, IonList, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonItem, IonList, IonLabel, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButtons, IonMenuButton, IonItem, IonList, IonLabel, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonItem, IonList, IonLabel, IonButton]
 })
 export class CartPage implements OnInit {
+  cartItems: { product: Product; quantity: number }[] = [];
 
   constructor(private cartService: CartService) {
     this.cartItems = this.cartService.getCart();
@@ -20,11 +21,10 @@ export class CartPage implements OnInit {
 
   ngOnInit() {
   }
-  cartItems: { product: Product; quantity: number }[] = [];
-
 
   removeFromCart(product: Product) {
     this.cartService.removeProduct(product);
+    this.cartItems = this.cartService.getCart(); // Actualizar los items del carrito
   }
 
   getTotalPrice() {
@@ -34,5 +34,4 @@ export class CartPage implements OnInit {
   getTotalQuantity() {
     return this.cartService.getTotalQuantity();
   }
-
 }
